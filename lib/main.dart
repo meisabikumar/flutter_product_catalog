73a54d1ad836fcb310cae7f6d823ee151e0e57f0
Abhishek Kumar <58994412+meisabikumar@users.noreply.github.com>
2021-12-10 00:24:05 +0530

    initial setup

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'pages/home_page.dart';
+
 void main() {
   runApp(const MyApp());
 }
@@ -13,13 +15,9 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.deepPurple,
       ),
-      home: Scaffold(
-        body: Center(
-          child: const Text('text'),
-        ),
-      ),
+      home: HomePage(),
     );
   }
 }
